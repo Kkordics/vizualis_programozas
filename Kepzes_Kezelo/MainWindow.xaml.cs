87a7ff7e0bb3bba3a06a257e0d8ec233e344ed74
Kkordics <75989127+Kkordics@users.noreply.github.com>
2025-03-27 19:52:23 +0100

    Alap wpd sprojekt hozzáadása

---
+++ Kepzes_Kezelo/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using cnKepzesek;
+namespace Kepzes_Kezelo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	/// 
+	public partial class MainWindow : Window
+	{
+		KepzesekTabla kepzesek;
+
+		public MainWindow()
+		{
+
+			InitializeComponent();
+			kepzesek = new KepzesekTabla();
+			Oktato oktato = new Oktato { Nev ="Sanyi", Szakterulet = "Sebész"};
+			
+			
+		}
+	}
+}
