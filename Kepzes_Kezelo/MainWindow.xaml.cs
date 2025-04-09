af28a7c8f9eac83af6addfe1141ac8610cb9cd15
Kkordics <75989127+Kkordics@users.noreply.github.com>
2025-04-09 14:30:37 +0200

    Javítás

--- Kepzes_Kezelo/MainWindow.xaml.cs
+++ Kepzes_Kezelo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +11,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using cnKepzesek;
+using Microsoft.EntityFrameworkCore;
 namespace Kepzes_Kezelo
 {
 	/// <summary>
@@ -18,16 +20,53 @@ namespace Kepzes_Kezelo
 	/// 
 	public partial class MainWindow : Window
 	{
-		KepzesekTabla kepzesek;
+		KepzesekTabla tabla;
 
 		public MainWindow()
 		{
 
 			InitializeComponent();
-			kepzesek = new KepzesekTabla();
-			Oktato oktato = new Oktato { Nev ="Sanyi", Szakterulet = "Sebész"};
+			tabla = new KepzesekTabla();
+			//Oktato oktato = new Oktato { Nev ="Sanyi", Szakterulet = "Sebész"};
+			Initdb();
 			
-			
+		}
+
+		private void Initdb()
+		{
+			tabla.Database.EnsureCreated();
+			if(tabla.Kepzesek != null)
+			{
+				
+				if (!tabla.Kepzesek.Any())
+				{
+					Populate();
+				}
+			}
+		}
+
+		private void Populate()
+		{
+			var k1 = new Kepzes { Hely = "Szeged", Nev = "ITBiz", BefejezesDatuma = DateTime.Now , KezdesDatuma = DateTime.Today};
+			var k2 = new Kepzes { Hely = "Pécs", Nev = "asd", BefejezesDatuma = DateTime.Now, KezdesDatuma = DateTime.Today };
+			var o1 = new Oktato { Kepzesek = [k1], Nev = "Saniy", Szakterulet = "Otthon" };
+
+			var r1 = new Resztvevo { Beosztas = "Kapu", Nev = "Péter", Kepzeseks = [k1, k2] };
+			var r2 = new Resztvevo { Beosztas = "Ajtó", Nev = "Sándor", Kepzeseks = [k1] };
+
+			k1.Oktatok.Add(o1);
+			k2.Oktatok.Add(o1);
+
+
+			tabla.Kepzesek.Add(k1);
+			tabla.Kepzesek.Add(k2);
+
+
+			tabla.Oktatok.Add(o1);
+			tabla.Resztvevok.Add(r1);
+			tabla.Resztvevok.Add(r2);
+			tabla.SaveChanges();
+
 		}
 
 		private void cAdd(object sender, RoutedEventArgs e)
@@ -42,8 +81,12 @@ namespace Kepzes_Kezelo
 			gAdd.Visibility = Visibility.Collapsed;
 			gVisualize.Visibility = Visibility.Visible;
 			gEdit.Visibility = Visibility.Collapsed;
-            //dgMegjelen.ItemsSource = cnKepzesek.Include(p => p.Oktatók).Include(p => p.Résztvevők).ToList();
-        }
+            dgMegjelen.ItemsSource = tabla.Kepzesek.Include(p => p.Oktatok).Include(p => p.Resztvevoks).ToList();
+			//dgMegjelen.ItemsSource = tabla.Kepzesek.ToList();
+			//KepzesOktato
+			Trace.WriteLine("asd"+string.Join(", ", tabla.Kepzesek.ToList()));
+
+		}
 
 		private void cEdit(object sender, RoutedEventArgs e)
 		{
@@ -51,10 +94,5 @@ namespace Kepzes_Kezelo
 			gVisualize.Visibility = Visibility.Collapsed;
 			gEdit.Visibility = Visibility.Visible;
 		}
-
-        private void cExit(object sender, RoutedEventArgs e)
-        {
-            Application.Current.Shutdown();
-        }
     }
 }
