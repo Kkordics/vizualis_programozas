d151dbcd7a6ddbb1db01fd2331560c237eb76e12
kom-andras <komlosi.andris@gmail.com>
2025-04-09 11:03:51 +0200

    KepzesKieg osztály

---
+++ Kepzes_Kezelo/KepzesKieg.cs
@@ -0,0 +1,16 @@
+﻿using cnKepzesek;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kepzes_Kezelo
+{
+    public partial class Kepzes
+    {
+        //public string? Oktato => Oktato?.Nev;
+        //public short? HelységIrányítószám => Oktato?.Irányítószám;
+        //public string Resztvevok => Resztvevo.Aggregate("", (c, a) => c + (c.Length > 0 ? ", " : "") + a.SzámSztring);
+    }
+}
